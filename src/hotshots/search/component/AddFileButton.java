92e551debf1516ac2b6367e2204c5cfa1d4ff6dc
Akpriest <Akpriest@192.168.0.7>
2016-02-24 20:49:41 -0500

    Added necessary imports to counter some debug errors in buttons and changed the exceptions in the buttons

--- src/hotshots/search/component/AddFileButton.java
+++ src/hotshots/search/component/AddFileButton.java
@@ -1,8 +1,13 @@
 package hotshots.search.component;
 
+import hotshots.search.Application.*;
+import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class AddFileButton extends JButton {
 	
@@ -11,15 +16,28 @@ public class AddFileButton extends JButton {
 		super("Add File...");
 		init();
 	}	
-			private void init(){
-				super.addActionListener(new ActionListener(){
+			private void init() {
+                            
+				super.addActionListener(new ActionListener() {
+                                            
+//Clicked Stub Method
+public void actionPerformed(ActionEvent e){
+// {//Executes when button is pressed
+                                              int i = 0;
+                                              
+                                              
+//	throw new Success("It Worked!");
+// }
+                                            try {
+                                                
+                                                
+                                                
+                                            } catch (Exception ex) {
+                                                System.out.println("Hello, World");
+                                                                                          
+                                            } 
 
-					//Clicked Stub Method
-					public void actionPerformed(ActionEvent e)
-					  {//Executes when button is pressed
-						throw new Success("It Worked!");
-					  }
-					
-				});
-			}
+                                        }
+                                        });
+}
 }
