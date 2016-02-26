0ffc2395ace831253174e36d7f2bdf3aeeb44673
Adam Craig <eod.craig@hotmail.com>
2016-02-25 22:03:37 -0500

    Update name on AboutButton Class

--- src/hotshots/search/component/AboutButton.java
+++ src/hotshots/search/component/AboutButton.java
@@ -7,7 +7,7 @@ import javax.swing.JButton;
 /**
  * HotShotsSearchEngine -- a basic search engine.
  * 
- * @author Adam Craig, Zack, Alex, and Alex
+ * @author Adam M Craig, Zack, Alex, and Alex
  *
  */
 public class AboutButton extends JButton {
