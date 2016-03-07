3c5bc4ccd7885a1d7dcd1e1e7ce48bd1be9e2080
Zacharyg95 <zachgay01@gmail.com>
2016-03-07 12:18:32 -0500

    Create new class file for creating the index
    made my method into a class just cause it makes more sense this way.

---
+++ src/hotshots/search/CreateIndex.java
@@ -0,0 +1,18 @@
+package hotshots.search.component;
+/**
+ * HotShotsSearchEngine -- a basic search engine.
+ * 
+ * @author Adam Craig, Zack, Alex, and Alex
+ *
+ */
+ public class CreateIndex {
+ 
+    //Method to create search indexes and will store in a file.
+    //Method is void at the moment but can return a pathname or value if needed.
+    public static void CreateIndex(){
+    	static HashMap<String, List<String>> index=new HashMap<String, List<String>>();
+    	//I believe this is the right type of object we need for the index.
+    }
+ 
+ 
+ }
