434861d3c8af16f8cefadc4f4b0f8023baff1f6c
Zachary <Zachgay01@gmail.com>
2016-03-06 22:46:14 -0500

    Added index object hashmap

--- src/hotshots/search/Application.java
+++ src/hotshots/search/Application.java
@@ -30,4 +30,11 @@ public class Application {
 	    super(message, throwable);
 	}
     }
+    
+    //Method to create search indexes and will store in a file.
+    //Method is void at the moment but can return a pathname or value if needed.
+    public static void CreateIndex(){
+    	static HashMap<String, List<String>> index=new HashMap<String, List<String>>();
+    	//I believe this is the right type of object we for the index.
+    }
 }
