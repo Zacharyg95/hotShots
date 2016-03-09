24d35822fe4b04d333ea2379b304f84ed3986795
Akpriest <Akpriest@192.168.0.7>
2016-03-09 13:37:04 -0500

    added feature to saveBeforeExit methode that converts string arrays into strings separated by commas

--- src/hotshots/search/model/SaveBeforeExit.java
+++ src/hotshots/search/model/SaveBeforeExit.java
@@ -25,16 +25,52 @@ public class SaveBeforeExit  {
      * @param fileName
      * @throws IOException
      */
-    public SaveBeforeExit(String fileName) throws IOException{
+    public static void SaveBeforeExit(String[] filenameArray, String[] pathnameArray, String[] invertedIndexArray, String[] indexArray) throws IOException{
+        
+        String filenames = "";
+        String pathnames = "";
+        String invertedIndexes = "";
+        String indexes = "";
+        
+        //loads filenameArray into one long string seperated by commas
+        for (int i = 0; i < filenameArray.length; i++){
+            
+           filenames += filenameArray[i]; 
+           filenames += ", ";
+        }
+        
+        //loads pathnameArray into one long string
+        for (int i = 0; i < pathnameArray.length; i++){
+            
+           pathnames += pathnameArray[i]; 
+           pathnames += ", ";
+        }
+        
+        //loads invertedIndexArray into one long string
+        for (int i = 0; i < invertedIndexArray.length; i++){
+            
+           invertedIndexes += invertedIndexArray[i]; 
+           invertedIndexes += ", ";
+        }
+        
+        //loads indexArray into one long string
+        for (int i = 0; i < indexArray.length; i++){
+            
+           indexes += indexArray[i]; 
+           indexes += ", ";
+        }
+        
         
         // creates properties instance
 	Properties properties = new Properties(); 
         
+        
         //create properties needed for values to be stored
-        properties.setProperty("index", "Test");
-        properties.setProperty("inverted-index", "Test");
-        properties.setProperty("path-names", "Test");
-        properties.setProperty("file-names", "Test");
+        properties.setProperty("index", indexes);
+        properties.setProperty("inverted-index", invertedIndexes);
+        properties.setProperty("path-names", pathnames);
+        properties.setProperty("file-names", filenames);
+       
        
         
         
@@ -42,11 +78,13 @@ public class SaveBeforeExit  {
        FileWriter writer = new FileWriter("conf.properties");
        
        //store data 
-       properties.store(writer, "Autoher: JP");
+       properties.store(writer, "Autoher: Alex, Zack, Adam, and Alex");
        
        // clsoe write
        writer.close();
     }
+    
+    
  
         
 
