e0890059c0d5194c395ef8f6ac77a6176eddc83a
Zachary Gay <zachgay01@gmail.com>
2016-04-20 23:29:32 -0400

    Update CreateIndex.java
    Format Fixes.

--- src/hotshots/search/component/CreateIndex.java
+++ src/hotshots/search/component/CreateIndex.java
@@ -25,41 +25,36 @@ public class CreateIndex {
    // Method will read the main file with the pathnames of the files that wish
    // to be indexed by the user.
 
-   public static void CreatePathNames(String FilesToBeIndexed) { // Method
-                                                                 // accepts
-                                                                 // pathname
-                                                                 // value to
-                                                                 // file with
-                                                                 // file to be
-                                                                 // indexed
+   public static void CreatePathNames(String FilesToBeIndexed) 
+      { // Method accepts pathname value to file with file to be indexed
       try {
-         File PassedFile = new File(FilesToBeIndexed);// Prepare file to be
-                                                      // scanned by Scanner
-         Scanner MainFile = new Scanner(PassedFile);// New Scanner to read the
-                                                    // passed parameter pathname
+            File PassedFile = new File(FilesToBeIndexed);// Prepare file to be
+                                                          // scanned by Scanner
+             Scanner MainFile = new Scanner(PassedFile);// New Scanner to read the
+                                                          // passed parameter pathname
 
-         int i = 0; // Create array index
-         while (MainFile.hasNext())// while the passed parameter has a next
-         // line this loop will run
-         {
-            Pathnames[i] = MainFile.next(); // stores value in pathname array.
-            i++; // Increment arry index for next value
-         }
-         MainFile.close(); // Clsoe scanner after while loop exits
-      } catch (FileNotFoundException e)// catches if it cannot find file for
-                                       // scanner
-      {
-         System.out.println("No File Was Found");
+             int i = 0; // Create array index
+             while (MainFile.hasNext())// while the passed parameter has a next
+                                       // line this loop will run
+               {
+                  Pathnames[i] = MainFile.next(); // stores value in pathname array.
+                   i++; // Increment arry index for next value
+                }
+            MainFile.close(); // Clsoe scanner after while loop exits
+            } 
+            catch (FileNotFoundException e)// catches if it cannot find file for scanner
+             {
+               System.out.println("No File Was Found.");
+             }
       }
-   }
 
-   public static void CreateSearchIndex(String[] Pathnames) {
-      public static String Words[];
+   public static void CreateSearchIndex(String[] Pathnames) 
+   {
+      public static String Words[]; //Method wide variable to hold words from indexing file
       for (String Path : Pathnames)// for loop for each pathname element
       {
          try {
-            File PassedFile = new File(Path);// Prepare file to be scanned by
-                                             // Scanner
+            File PassedFile = new File(Path);// Prepare file to be scanned by Scanner
             FileInputStream fis = new FileInputStream(PassedFile); // read contents of the new file
             BufferedInputStream  bis = new BufferedInputStream(fis);// buffer decrease reading time
             DataInputStream dis = new DataInputStream(bis);// Stream to read data to variable
@@ -84,14 +79,12 @@ public class CreateIndex {
           bis.close();//close to reopen at start of next loop
           dis.close();// close after data has been read and saved
          }
-
-         catch (FileNotFoundException e)// catches if it cannot find file for
-                                        // scanner
+         catch (FileNotFoundException e)// catches if it cannot find file for scanner
          {
-            System.out.println("No File Was Found");
+            System.out.println("No File Was Found.");
          } catch (IOException e)// catches if it cannot find file for scanner
          {
-           System.out.println("No File Was Selected");
+           System.out.println("No File Was Selected.");
          }
       }
    }
